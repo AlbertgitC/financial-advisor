3464685835cfd0728e68e22b57f825c2413f4ed6
Albert Cheng <lptalbert@gmail.com>
2021-01-21 04:42:04 -0800

    delete unneeded file

--- src/components/util/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-import rootReducer from './reducer';
-
-const configureStore = (preloadedState = {}) => (
-    createStore(
-        rootReducer,
-        preloadedState
-    )
-);
