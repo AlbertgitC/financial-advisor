3a8facc46a6942903d28e8b9980fcd3ed78d067c
Albert Cheng <lptalbert@gmail.com>
2021-01-22 13:27:27 -0800

    config # route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import DonutGraph from './components/donutGraph';
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import CalInvest from './components/calInvest';
 
 function App() {
 
   return (
     <div className="App">
+      <Redirect from="/" to="/chart" />
       <div className="header">
         <h1>Financial Advisor</h1>
       </div>
